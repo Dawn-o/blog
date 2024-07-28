36a11738980571e7b1c5d53d1bbe610f50296073
Rushel <ruselaq@gmail.com>
2024-07-28 23:19:33 +0800

    Redesign UI

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Post::factory(100)->recycle([
+            Category::factory(4)->create(),
+            User::factory(7)->create()
+        ])->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,42 @@
 <?php
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('home', ['title' => 'Home']);
+    return view('pages.home', ['title' => 'Home']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['title' => 'About']);
+    return view('pages.about', ['title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog',['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('pages.posts', [
+        'title' => 'Blog',
+        'posts' => Post::all(),
+    ]);
+});
+
+Route::get('/posts/{post:slug}', function (Post $post) {
+
+    return view('pages.post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/authors/{user:username}', function (User $user) {
+
+    return view('pages.posts', ['title' => count($user->posts) . ' Article by '. $user->name, 'posts' => $user->posts]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+
+    return view('pages.posts', ['title' => ' Articles in: '. $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {
-    return view('contact', ['title' => 'Contact']);
+    return view('pages.contact', ['title' => 'Contact']);
 });
